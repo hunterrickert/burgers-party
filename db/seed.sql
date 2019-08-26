2c63850a5885b7835c98db5b0ef3f774c1651bf0
Hunter Rickert <hunter.rickert@gmail.com>
2019-08-26 09:52:05 -0700

    changing incorrect variable names

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, devoured) VALUES ('peanut butter burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('cheese burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('bbq burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('coffe burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('trash burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('brittney burger', false);
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,60 @@
+$(function() {
+    $(".change-devour").on("click", function(event) {
+      var id = $(this).data("id");
+      var newdevour = $(this).data("newdevour");
+  
+      var newDevourState = {
+        devoured: newdevour
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burger/" + id, {
+        type: "PUT",
+        data: newDevourState
+      }).then(
+        function() {
+          console.log("current burger state is ", newSleep);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        sleepy: $("[name=devoured]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("made new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted burger", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
